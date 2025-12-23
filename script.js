a76cf04e6c229474824a37b609b8ec7538d111e2
Tojo-glitch <chanon.chonv@gmail.com>
2025-12-23 15:27:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("snow");
 const ctx = canvas.getContext("2d");
 
-let w, h, snowflakes = [];
+let w, h;
+const flakes = [];
 
 function resize() {
   w = canvas.width = window.innerWidth;
@@ -11,26 +12,25 @@ window.addEventListener("resize", resize);
 resize();
 
 for (let i = 0; i < 120; i++) {
-  snowflakes.push({
+  flakes.push({
     x: Math.random() * w,
     y: Math.random() * h,
     r: Math.random() * 2 + 1,
-    s: Math.random() * 1 + 0.5
+    s: Math.random() + 0.5
   });
 }
 
-function snow() {
+function draw() {
   ctx.clearRect(0,0,w,h);
   ctx.fillStyle = "rgba(255,255,255,.8)";
-  snowflakes.forEach(f => {
+  flakes.forEach(f => {
     ctx.beginPath();
     ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2);
     ctx.fill();
     f.y += f.s;
     if (f.y > h) f.y = 0;
   });
-  requestAnimationFrame(snow);
+  requestAnimationFrame(draw);
 }
-snow();
-// intentionally minimal
-console.log("3650 Dayairlines ready");
+
+draw();
